30a2cb01a05d3aea81c6576aa30f59abf8552f81
Aleksandr Sobol <aleksandr.sobol@ligastavok.ru>
2019-10-16 11:51:18 +0300

    added translation dao test

---
+++ app/src/androidTest/java/ru/xsobolx/dictionary/data/database/TestData.kt
@@ -0,0 +1,32 @@
+package ru.xsobolx.dictionary.data.database
+
+import ru.xsobolx.dictionary.data.db.translation.model.DictionaryDBModel
+import ru.xsobolx.dictionary.data.network.translation.TranslationResponse
+import ru.xsobolx.dictionary.domain.translation.model.DictionaryEntry
+import ru.xsobolx.dictionary.domain.translation.model.Language
+import ru.xsobolx.dictionary.domain.translation.model.TranslatedWord
+
+val testEntry = DictionaryEntry(
+    word = "test",
+    translation = "тест",
+    fromLanguage = Language.EN,
+    toLanguage = Language.RU,
+    isFavorite = false
+)
+val testTranslatedWord = TranslatedWord(
+    word = "test",
+    fromLanguage = Language.EN,
+    toLanguage = Language.RU
+)
+val testTranslationResponse = TranslationResponse(
+    code = 200,
+    lang = "en-ru",
+    text = listOf("тест")
+)
+val testDictionaryDataBaseModel = DictionaryDBModel(
+    word = "test",
+    fromLanguage = Language.EN,
+    toLanguage = Language.RU,
+    translation = "тест",
+    isFavorite = true
+)
---
+++ app/src/androidTest/java/ru/xsobolx/dictionary/data/database/TranslationDAOTest.kt
@@ -0,0 +1,53 @@
+package ru.xsobolx.dictionary.data.database
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import io.reactivex.observers.TestObserver
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import ru.xsobolx.dictionary.data.db.base.AppDatabase
+import ru.xsobolx.dictionary.data.db.translation.dao.TranslationDAO
+import ru.xsobolx.dictionary.data.db.translation.model.DictionaryDBModel
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class TranslationDAOTest {
+
+    @Rule
+    @JvmField
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var translationDAO: TranslationDAO
+    private lateinit var testSubscriber: TestObserver<DictionaryDBModel>
+    private lateinit var db: AppDatabase
+
+    @Before
+    fun setUp() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        testSubscriber = TestObserver()
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+        translationDAO = db.translationDao()
+    }
+
+    @After
+    fun tearDown() {
+        db.close()
+    }
+
+    @Test
+    fun getAllEntriesAfterInsert() {
+        translationDAO.insertDictionaryEntry(testDictionaryDataBaseModel)
+
+        val entries = translationDAO.loadAllDictionaryEntries()
+        entries.subscribe(testSubscriber)
+
+        testSubscriber.assertResult(testDictionaryDataBaseModel)
+    }
+}
