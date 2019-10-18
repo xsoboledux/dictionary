1c87ccc1aeda25c994c8fdee8c824d3549cab4ca
Aleksandr Sobol <aleksandr.sobol@ligastavok.ru>
2019-10-18 14:31:49 +0300

    implemented SetLanguageUseCase

---
+++ app/src/test/java/ru/xsobolx/dictionary/domain/translation/SetLanguageUseCaseTest.kt
@@ -0,0 +1,38 @@
+package ru.xsobolx.dictionary.domain.translation
+
+import io.reactivex.Completable
+import io.reactivex.observers.TestObserver
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
+import ru.xsobolx.dictionary.data.repositories.translation.LanguageRepository
+import ru.xsobolx.dictionary.domain.translation.model.LanguageEntity
+
+class SetLanguageUseCaseTest {
+
+    @Mock
+    private lateinit var languageRepository: LanguageRepository
+    private lateinit var setLanguageUseCase: SetLanguageUseCase
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        setLanguageUseCase = SetLanguageUseCase.Impl(languageRepository)
+    }
+
+    @Test
+    fun shouldSetLanguageAndReturnThisLanguage() {
+        val actualLanguageEntity = testFromLanguageEntity
+        `when`(languageRepository.setLanguage(actualLanguageEntity)).thenReturn(Completable.complete())
+
+        val testSubscriber = TestObserver<LanguageEntity>()
+        val actual = setLanguageUseCase.execute(actualLanguageEntity)
+        actual.subscribe(testSubscriber)
+
+        verify(languageRepository, times(1)).setLanguage(actualLanguageEntity)
+        val expected = testFromLanguageEntity.copy()
+        testSubscriber.assertResult(expected)
+    }
+}
